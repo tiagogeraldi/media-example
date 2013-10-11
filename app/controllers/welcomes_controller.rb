d6d2f089c377d98c674f8c085c42756bd40bf606
Tiago A. Geraldi <virgulla@gmail.com>
2013-10-11 15:24:29 -0300

    Adding video upload

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,7 +1,23 @@
 class WelcomesController < ApplicationController
+  before_filter :authenticate_user!, :only => [:new, :create]
+
   def index
     @album = Chute::V2::Albums.all.data[0]
 
     @assets = Chute::V2::Assets.all(@album.id)
+  end
+
+  def new
+  end
+
+  def create
+    album = Chute::V2::Albums.all.data[0]
+    Chute::V2::Assets.upload(album.id, params[:video][:video])
+
+    redirect_to welcomes_path
+  end
+
+  def show
+    @asset = Chute::V2::Assets.find(params[:id])
   end
 end
